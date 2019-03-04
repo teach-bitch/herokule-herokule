30f149a4c830b6970be8fa0bc89ff7ac49d9a29c
kokogakayui <kokogakayui@gmail.com>
2019-03-04 14:40:06 +0100

    KitUI Plugged

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require activestorage
 //= require_tree .
+//= require js/core/jquery.3.2.1.min
+//= require js/core/popper.min
+//= require js/core/bootstrap.min
+//= require js/plugins/moment.min
+
+//= require js/plugins/bootstrap-switch
+//= require js/plugins/bootstrap-tagsinput
+
+//= require js/plugins/bootstrap-selectpicker
+
+//= require js/plugins/jasny-bootstrap.min
+
+//= require js/plugins/nouislider.min
+
+//= require js/plugins/bootstrap-datetimepicker.min
+
+//= require js/now-ui-kit
+
+//= require js/plugins/presentation-page/rellax.min
