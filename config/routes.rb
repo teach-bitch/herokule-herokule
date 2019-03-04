c483a04c5ca0abd7aa63afb8eaf7d06f3dcdf175
Yacinth <fevriercjjc@gmail.com>
2019-03-05 00:14:32 +0100

    wip view show#product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "products#index"
+<<<<<<< HEAD
   get '/home/index'	
   devise_for :users
+=======
+>>>>>>> wip view show#product
 
   resources :line_items, only: [:create, :edit, :update, :destroy, :new]
   resources :facturations, except: [:destroy ,:new]
