59f751c79d81544345fd9428a0b15fc4bfa16472
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-05 14:21:23 +0100

    fix :new routes + removed json files

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root "products#index"
   devise_for :users
 
-  resources :line_items, only: [:create, :edit, :update, :destroy]
-  resources :facturations, except: [:destroy]
-  resources :baskets, except: [:index]
+  resources :line_items, only: [:create, :edit, :update, :destroy, :new]
+  resources :facturations, except: [:destroy ,:new]
+  resources :baskets, except: [:index ,:new]
   resources :categories
   resources :products
 end
