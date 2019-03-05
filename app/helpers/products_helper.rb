ced17095ff73a06192989a7f55b9fa32ee0f98f5
Yacinth <fevriercjjc@gmail.com>
2019-03-05 03:25:35 +0100

    update show#product, test products controller and products_helper, test product.scss and application.css

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  #je créer une methode pour trouver les produits ayant la même categorie que le produit sur la view show
+  def other_product(category)
+    Product.find_by(category_id: category)
+      puts ".O-P."*70
+  end
 end
