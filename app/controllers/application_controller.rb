b2f668b00ce0f5f0733aad479eb6a00c354f23b1
kokogakayui <kokogakayui@gmail.com>
2019-03-06 20:17:34 +0100

    ajax lineitem working except for full total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-=======
 	include CurrentBasket
 	before_action :set_basket
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,5 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :birthdate])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :birthdate])
   end
->>>>>>> master
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
-<<<<<<< HEAD
-
-=======
 	def set_basket
 		@basket = Basket.find_by(id: session[:basket_id]) || Basket.create(user_id: current_user.id)
 		session[:basket_id] ||= @basket.id
 	end
->>>>>>> master
 end
