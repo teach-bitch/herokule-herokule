c4db4806696f1af6bdee25acbce7f376eea68ce1
Yacinth <fevriercjjc@gmail.com>
2019-03-06 12:37:32 +0100

    try to find a method for product filter by category

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,10 +1,10 @@
 module ProductsHelper
   def category_helper
     puts @product.title
-    all_categories = @product.product_categories
+    all_categories_of_product_show = @product.product_categories
       puts all_categories
       puts "all cat" * 10
-      all_categories.each do |c|
+      all_categories_of_product_show.each do |c|
       @cat_id = c.category_id
       puts c.category_id
       puts "c" * 10
@@ -19,15 +19,25 @@ module ProductsHelper
   end
 
   
-#   def all_cat_prod  
+  def all_prod_cat(cat_id)  
       
-#     @cat_all_product = Category.find_by(id: @cat_id).title
+    @cat_all_product = Category.find_by(id:cat_id)
+    @product_cat = ProductCategory.find_by()
+    @product_same_cat = Product.find_by(category_id:)
 
-#         puts "cat all product" * 10
-#         puts @cat_all_product
-#         puts "cat all product" * 10
-#   end
-# end
+        puts "cat all product" * 10
+        puts @cat_all_product
+        puts "cat all product" * 10
+  end
+
+  def similar_product
+    puts @product.categories
+      @similar_products = @product.categories.inject({}) do |result_hash, category|
+      result_hash[category.name.to_sym] => category.products.reject {|p| p == @product}
+      result_hash
+      end
+    end
+  end
     
 
 
