5106045928be6ccd341c86653890359a358b3863
willyspa <armed95@hotmail.fr>
2019-03-07 21:31:13 +0100

    ajout index products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,12 +3,13 @@ class Admin::ProductsController < AdminController
 
   # Methods omitted
   def index
-    
+
     puts "$" * 60
     puts "PRODUCT ADMIN INDEX"
     puts params
     puts "$" * 60
-    @product = Product.all
+    @products = Product.all
+    @categories = Category.all
 
   end
 
