6cf57b211643fc2541be4cce35210bab6e398029
kokogakayui <kokogakayui@gmail.com>
2019-03-07 23:25:22 +0100

    facturation index ok

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ end
   devise_for :users
 
   resources :line_items, only: [:create, :update, :destroy]
-  resources :facturations, only: [:show]
+  resources :facturations, only: [:show, :index]
   resources :baskets, only: [:create, :update, :destroy, :show]
     resources :charges
   resources :categories, only: [:create, :update, :destroy, :show, :index]
