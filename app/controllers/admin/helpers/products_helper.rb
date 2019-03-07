2f5ae3f2fffcbf1230ede896b08f45b4d529d940
willyspa <armed95@hotmail.fr>
2019-03-07 22:26:26 +0100

    ajout show

---
+++ app/controllers/admin/helpers/products_helper.rb
@@ -0,0 +1,19 @@
+module AdminProductsHelper
+
+  def set_cats
+    @all_cat_categories = @product.categories # toutes les catégories auxquelles le chat appartient
+    puts "set_cats" * 30
+    puts @all_cat_categories
+    puts "set_cats" * 30
+
+    @the_cats = []
+
+    @all_cat_categories.each do |category|
+      cats = category.products # tout les chats appartenant à la catégorie de la boucle
+      cats.each do |cat|
+        @the_cats << cat
+      end
+    end
+    puts @the_cats
+  end
+end
