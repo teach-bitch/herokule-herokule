89126d1aa1eb99e8cab72a76f29ed85ebb820f93
kokogakayui <kokogakayui@gmail.com>
2019-03-07 14:09:17 +0100

    routes.rb improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root "products#index"
-  get '/home/index'	
+  get '/home/index'
   devise_for :users
 
-  resources :line_items, only: [:create, :edit, :update, :destroy, :new]
+  resources :line_items, only: [:create, :update, :destroy]
   resources :facturations, except: [:destroy ,:new]
-  resources :baskets, except: [:index ,:new]
-  resources :categories
+  resources :baskets, only: [:create, :update, :destroy, :show]
+  resources :categories, only: [:create, :update, :destroy, :show, :index]
   resources :products
 end
