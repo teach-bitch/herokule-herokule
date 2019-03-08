5177e5eb079cd2351db4dd541c9eb559d22db7de
willyspa <armed95@hotmail.fr>
2019-03-08 04:24:59 +0100

    ajout form edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_165735) do
+ActiveRecord::Schema.define(version: 2019_03_07_154132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,17 +59,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_165735) do
     t.index ["user_id"], name: "index_facturations_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "line_items", force: :cascade do |t|
     t.bigint "basket_id"
     t.bigint "product_id"
@@ -98,8 +87,6 @@ ActiveRecord::Schema.define(version: 2019_03_07_165735) do
     t.string "reference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "slug"
-    t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -115,10 +102,8 @@ ActiveRecord::Schema.define(version: 2019_03_07_165735) do
     t.string "last_name"
     t.string "username"
     t.datetime "birthdate"
-    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
