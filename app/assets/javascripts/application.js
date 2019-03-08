76386f2c6759d1f7d983a0291278d019cbb70d15
kokogakayui <kokogakayui@gmail.com>
2019-03-08 11:36:12 +0100

    tesst

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,34 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery3
+//= require jquery_ujs
+//= require_tree .
+//= require js/core/jquery.3.2.1.min
+//= require js/core/popper.min
+//= require js/core/bootstrap.min
+//= require js/plugins/moment.min
+
+//= require js/plugins/bootstrap-switch
+//= require js/plugins/bootstrap-tagsinput
+
+//= require js/plugins/bootstrap-selectpicker
+
+//= require js/plugins/jasny-bootstrap.min
+
+//= require js/plugins/nouislider.min
+
+//= require js/plugins/bootstrap-datetimepicker.min
+
+//= require js/now-ui-kit
+
+//= require js/plugins/presentation-page/rellax.min
