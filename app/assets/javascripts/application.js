7f09e39f72cbdc153b3d17585722a53e988078b4
kokogakayui <kokogakayui@gmail.com>
2019-03-08 09:16:43 +0100

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require jquery_ujs
 //= require_tree .
 //= require js/core/jquery.3.2.1.min
